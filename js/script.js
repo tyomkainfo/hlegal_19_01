7cf450b8a84307ca17e6a7e121288995531848b8
VitLozov <v.lozovskyi@gmail.com>
2022-04-22 23:02:29 +0300

    Homepage 98% done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $('.navbar-brand').click(function() {
     $('.header').toggleClass('white');
 });
+
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1000: {
+                items: 1
+            }
+        }
+    });
+});
